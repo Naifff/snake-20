188806e4c874ac26e1dfde89fc7632af434140be
Naif <knifffe@yandex.ru>
2022-12-20 10:14:55 +0300

    3

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let snake = [game.createSprite(2, 2), game.createSprite(1, 2), game.createSprite(0, 2)]
 basic.forever(function () {
-	
+    for (let index = 0; index <= snake.length - 1; index++) {
+        if (snake[index].get(LedSpriteProperty.X) == 4) {
+            snake[index].set(LedSpriteProperty.X, 0)
+        } else if (snake[index].get(LedSpriteProperty.Y) == 4) {
+            snake[index].set(LedSpriteProperty.Y, 0)
+        } else {
+            snake[index].move(1)
+        }
+    }
+    basic.pause(200)
 })
