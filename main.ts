b6856345accaac26d20e8e1565029df28b211611
Naif <knifffe@yandex.ru>
2022-12-20 12:40:03 +0300

    2

--- main.ts
+++ main.ts
@@ -20,12 +20,11 @@ buttonClicks.onButtonDown(buttonClicks.AorB.A, function () {
         snake[0].turn(Direction.Left, 90)
     }
 })
-let n = 0
+let t = 0
+let tt = 0
 let snake: game.LedSprite[] = []
 snake = [game.createSprite(2, 2), game.createSprite(1, 2), game.createSprite(0, 2)]
-snake[0].turn(Direction.Right, -90)
 basic.forever(function () {
-    n = 0
     for (let index = 0; index <= snake.length - 1; index++) {
         if (snake[index].get(LedSpriteProperty.X) == 4 && snake[index].get(LedSpriteProperty.Direction) == 90) {
             snake[index].set(LedSpriteProperty.X, 0)
@@ -35,14 +34,16 @@ basic.forever(function () {
             snake[index].set(LedSpriteProperty.Y, 4)
         } else if (snake[index].get(LedSpriteProperty.Y) == 4 && snake[index].get(LedSpriteProperty.Direction) == 180) {
             snake[index].set(LedSpriteProperty.Y, 0)
-        } else if (index != 0 && n == 0) {
-            snake[index].move(1)
-            if (snake[index].get(LedSpriteProperty.Direction) != snake[index - 1].get(LedSpriteProperty.Direction)) {
+        } else if (index != 0) {
+            tt = snake[index].get(LedSpriteProperty.Direction)
+            if (snake[index].get(LedSpriteProperty.Direction) != t) {
                 snake[index].set(LedSpriteProperty.Direction, snake[index - 1].get(LedSpriteProperty.Direction))
-                n += 1
             }
+            snake[index].move(1)
+            t = tt
         } else {
             snake[index].move(1)
+            t = snake[index].get(LedSpriteProperty.Direction)
         }
     }
     basic.pause(1000)
