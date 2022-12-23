7e0827ef4b9c403d64905dacc16f1fc6e220fbd3
Naif <knifffe@yandex.ru>
2022-12-23 13:12:40 +0300

    not work turn

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ buttonClicks.onButtonDown(buttonClicks.AorB.B, function () {
     }
 })
 function sync () {
-    for (let index = 0; index <= snake.length - 2; index++) {
+    for (let index = 0; index <= snake.length - 1; index++) {
         list[index] = snake[index].get(LedSpriteProperty.Direction)
     }
 }
@@ -40,7 +40,8 @@ buttonClicks.onButtonDown(buttonClicks.AorB.A, function () {
 })
 function move () {
     sync()
-    basic.pause(1000)
+    tail = game.createSprite(snake[snake.length - 1].get(LedSpriteProperty.X), snake[snake.length - 1].get(LedSpriteProperty.Y))
+    tail.change(LedSpriteProperty.Direction, snake[snake.length - 1].get(LedSpriteProperty.Direction))
     for (let index = 0; index <= snake.length - 2; index++) {
         snake[index + 1].set(LedSpriteProperty.Direction, list[index])
     }
@@ -58,10 +59,14 @@ function move () {
         }
     }
     if (food.isTouching(snake[0])) {
-        snake.unshift(game.createSprite(food.get(LedSpriteProperty.X), food.get(LedSpriteProperty.Y)))
+        snake.insertAt(snake.length - 0, tail)
         newFood()
+    } else {
+        tail.delete()
     }
+    basic.pause(1000)
 }
+let tail: game.LedSprite = null
 let ok = 0
 let list: number[] = []
 let food: game.LedSprite = null
